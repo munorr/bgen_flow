012817f2877f3274df11419f60c0745af6689207
munorr <137064603+munorr@users.noreply.github.com>
2023-06-19 23:37:26 -0500

    Updated for 4.0 Alpha

--- __init__.py
+++ __init__.py
@@ -2226,7 +2226,7 @@ class BGEN_preferences(bpy.types.AddonPreferences):
     auto_check_update = bpy.props.BoolProperty(
         name="Auto-check for Update",
         description="If enabled, auto-check for updates using an interval",
-        default=False)
+        default=True)
 
     updater_interval_months = bpy.props.IntProperty(
         name='Months',
